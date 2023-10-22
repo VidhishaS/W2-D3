faff98ec09804bf1831931c74d89533cb26c56ca
Vidhisha Shetty <109327902+VidhishaS@users.noreply.github.com>
2023-10-22 20:42:09 +0530

    Add files via upload

---
+++ Body.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const Body = () => {
+    return(
+        <>
+        <hr />
+        <h3>About me</h3>
+        <p>
+        Sudha Murty is an Indian educator, author and philanthropist who is chairperson of the Infosys Foundation. She is married to the co-founder of Infosys, N. R. Narayana Murty. Murty was awarded the Padma Shri, the fourth highest civilian award in India, for social work by the Government of India in 2006. Later in 2023, she was awarded the Padma Bhushan, the third highest civilian award in India.
+        </p>
+ </>
+    );
+};
+export default Body; 
---
+++ Footer.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+class Footer extends Component {
+    render() {
+        return(
+            <>
+            <hr/>
+            <h3>Contact</h3>
+            <p>
+                <b>Mobile</b> 9483336155
+            </p>
+            <p>
+                <b>Email</b> sudmur@gmail.com
+            </p>
+            <p>
+                <b><a href="https://www.linkedin.com/search/results/all/?keywords=sudha%20murthy&origin=GLOBAL_SEARCH_HEADER&sid=t4w" >LinkedIn</a></b>
+            </p>
+            </>
+        );
+    };
+};
+export default Footer;
